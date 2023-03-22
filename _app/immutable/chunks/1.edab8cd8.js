afc911582ccd9a42a2c5907500f5457e6ee5e89b
Marco Crespi <mcrespi@tremor.co>
2023-03-22 12:25:08 +0100

    Updates

--- _app/immutable/chunks/1.edab8cd8.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.e783215a.js";export{t as component};
---
+++ _app/immutable/chunks/2.35b9c8ab.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.5dc2acb4.js";export{t as component};
